0d07be837b3b330f79b1db877c45ce713aa05fb7
takechee <takechee14@gmail.com>
2019-11-30 22:12:09 +0900

    Fragmentへ移行

--- app/src/main/java/com/takechee/qrcodereader/MainActivity.kt
+++ app/src/main/java/com/takechee/qrcodereader/MainActivity.kt
@@ -1,36 +1,13 @@
 package com.takechee.qrcodereader
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.net.toUri
-import com.google.zxing.integration.android.IntentIntegrator
-import com.journeyapps.barcodescanner.DecoratedBarcodeView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val intentIntegrator = IntentIntegrator(this).apply {
-            captureActivity = CustomCaptureActivity::class.java
-        }
-
-        intentIntegrator.initiateScan()
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        when (val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)) {
-            null -> super.onActivityResult(requestCode, resultCode, data)
-            else -> when (val contents = result.contents) {
-                null -> Toast.makeText(this, "", Toast.LENGTH_SHORT).show()
-                else -> {
-                    val intent = Intent(Intent.ACTION_VIEW, contents.toUri())
-                    startActivity(intent)
-                }
-            }
-        }
-    }
 }
---
+++ app/src/main/java/com/takechee/qrcodereader/ui/home/HomeFragment.kt
@@ -0,0 +1,62 @@
+package com.takechee.qrcodereader.ui.home
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.net.toUri
+import androidx.fragment.app.Fragment
+import com.google.zxing.integration.android.IntentIntegrator
+import com.takechee.qrcodereader.CustomCaptureActivity
+import com.takechee.qrcodereader.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    private val intentIntegrator: IntentIntegrator by lazy {
+        IntentIntegrator.forSupportFragment(this).apply {
+            captureActivity = CustomCaptureActivity::class.java
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        intentIntegrator.initiateScan()
+
+        binding.openReaderButton.setOnClickListener {
+            intentIntegrator.initiateScan()
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)) {
+            null -> super.onActivityResult(requestCode, resultCode, data)
+            else -> when (val contents = result.contents) {
+                null -> Toast.makeText(context, "canceled", Toast.LENGTH_SHORT).show()
+                else -> {
+                    val intent = Intent(Intent.ACTION_VIEW, contents.toUri())
+                    startActivity(intent)
+                }
+            }
+        }
+    }
+}
