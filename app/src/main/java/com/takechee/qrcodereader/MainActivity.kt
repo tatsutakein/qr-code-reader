f7ad2ca9e4cbca48a86dd8cf58aec341734b841e
takechee <takechee14@gmail.com>
2019-11-30 20:45:38 +0900

    読み取り結果を受け取ってActivityを起動するところまで

--- app/src/main/java/com/takechee/qrcodereader/MainActivity.kt
+++ app/src/main/java/com/takechee/qrcodereader/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.takechee.qrcodereader
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 import com.google.zxing.integration.android.IntentIntegrator
+import com.journeyapps.barcodescanner.DecoratedBarcodeView
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,5 +19,18 @@ class MainActivity : AppCompatActivity() {
         }
 
         intentIntegrator.initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)) {
+            null -> super.onActivityResult(requestCode, resultCode, data)
+            else -> when (val contents = result.contents) {
+                null -> Toast.makeText(this, "", Toast.LENGTH_SHORT).show()
+                else -> {
+                    val intent = Intent(Intent.ACTION_VIEW, contents.toUri())
+                    startActivity(intent)
+                }
+            }
+        }
     }
 }
