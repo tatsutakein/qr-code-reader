3fbbb7e4afdf9b34880d90c5736f0f88bd1d7b5a
takechee <takechee14@gmail.com>
2019-11-30 12:18:04 +0900

    一旦起動まで

--- app/src/main/java/com/takechee/qrcodereader/MainActivity.kt
+++ app/src/main/java/com/takechee/qrcodereader/MainActivity.kt
@@ -2,6 +2,7 @@ package com.takechee.qrcodereader
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.integration.android.IntentIntegrator
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        IntentIntegrator(this).initiateScan()
 //        GlobalScope.launch {  }
     }
 }
