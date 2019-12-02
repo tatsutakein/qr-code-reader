f472af52967988f1ca9a0cb5494102eb3bb977ab
takechee <takechee14@gmail.com>
2019-12-02 22:24:28 +0900

    ViewModelFactory

--- app/src/main/java/com/takechee/qrcodereader/di/AppComponent.kt
+++ app/src/main/java/com/takechee/qrcodereader/di/AppComponent.kt
@@ -12,6 +12,7 @@ import javax.inject.Singleton
     modules = [
         AndroidSupportInjectionModule::class,
         AppModule::class,
+        ViewModelModule::class,
         ActivityBindingModule::class
     ]
 )
---
+++ app/src/main/java/com/takechee/qrcodereader/di/ViewModelModule.kt
@@ -0,0 +1,11 @@
+package com.takechee.qrcodereader.di
+
+import androidx.lifecycle.ViewModelProvider
+import dagger.Binds
+import dagger.Module
+
+@Module
+abstract class ViewModelModule {
+    @Binds
+    internal abstract fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
+}
