36f6c9155dcf2b7ba71521b090706e8d6040a344
takechee <takechee14@gmail.com>
2019-12-07 18:01:12 +0900

    repository

---
+++ app/src/main/java/com/takechee/qrcodereader/data/repository/ReadCodeRepository.kt
@@ -0,0 +1,13 @@
+package com.takechee.qrcodereader.data.repository
+
+import javax.inject.Inject
+
+interface ReadCodeRepository {
+}
+
+
+class ReadCodeDataRepository @Inject constructor(
+    
+) : ReadCodeRepository {
+
+}
