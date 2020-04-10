2939ae13d76148144009628534ad904830db0d8b
takechee <takechee14@gmail.com>
2020-04-11 08:33:34 +0900

    依存ライブラリのバージョンアップ

--- buildSrc/src/main/java/dependencies/Dependencies.kt
+++ buildSrc/src/main/java/dependencies/Dependencies.kt
@@ -3,31 +3,31 @@ package dependencies
 @Suppress("unused")
 object Dependencies {
     object Kotlin {
-        private const val version = "1.3.60"
+        private const val version = "1.3.71"
         const val plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
         const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:$version"
     }
 
     object AndroidX {
+        const val coreKtx = "androidx.core:core-ktx:1.2.0"
         const val appcompat = "androidx.appcompat:appcompat:1.1.0"
-        const val fragment = "androidx.fragment:fragment:1.2.0-rc02"
-        const val coreKtx = "androidx.core:core-ktx:1.1.0"
-        const val constraintLayout = "androidx.constraintlayout:constraintlayout:1.1.3"
+        const val fragment = "androidx.fragment:fragment-ktx:1.2.4"
+        const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.0-beta4"
+        const val recyclerview = "androidx.recyclerview:recyclerview:1.2.0-alpha02"
         const val browser = "androidx.browser:browser:1.0.0"
         const val preferenceKtx = "androidx.preference:preference-ktx:1.1.0"
-        const val material = "com.google.android.material:material:1.2.0-alpha01"
+        const val material = "com.google.android.material:material:1.2.0-alpha05"
 
         object Lifecycle {
-            private const val version = "2.2.0-rc02"
-            const val extensions = "androidx.lifecycle:lifecycle-extensions:$version"
+            private const val version = "2.2.0"
             const val compiler = "androidx.lifecycle:lifecycle-compiler:$version"
             const val liveDataKtx = "androidx.lifecycle:lifecycle-livedata-ktx:$version"
             const val viewModelKtx = "androidx.lifecycle:lifecycle-viewmodel-ktx:$version"
-            const val savedState = "androidx.lifecycle:lifecycle-viewmodel-savedstate:1.0.0-rc02"
+            const val savedState = "androidx.lifecycle:lifecycle-viewmodel-savedstate:$version"
         }
 
         object Navigation {
-            private const val version = "2.2.0-rc02"
+            private const val version = "2.2.1"
             const val safeArgsPlugin =
                 "androidx.navigation:navigation-safe-args-gradle-plugin:$version"
             const val fragment = "androidx.navigation:navigation-fragment-ktx:$version"
@@ -35,7 +35,7 @@ object Dependencies {
         }
 
         object Room {
-            private const val version = "2.2.2"
+            private const val version = "2.2.5"
             const val ktx = "androidx.room:room-ktx:${version}"
             const val runtime = "androidx.room:room-runtime:${version}"
             const val compiler = "androidx.room:room-compiler:${version}"
@@ -43,13 +43,13 @@ object Dependencies {
         }
 
         object Licenses {
-            const val gradlePlugin = "com.google.android.gms:oss-licenses-plugin:0.10.0"
+            const val gradlePlugin = "com.google.android.gms:oss-licenses-plugin:0.10.2"
             const val core = "com.google.android.gms:play-services-oss-licenses:17.0.0"
         }
     }
 
     object Dagger {
-        private const val version = "2.24"
+        private const val version = "2.27"
         const val android = "com.google.dagger:dagger-android:$version"
         const val androidSupport = "com.google.dagger:dagger-android-support:$version"
         const val compiler = "com.google.dagger:dagger-compiler:$version"
@@ -73,5 +73,5 @@ object Dependencies {
         const val dataBinding = "com.xwray:groupie-databinding:$version"
     }
 
-    const val threeten = "com.jakewharton.threetenabp:threetenabp:1.2.1"
+    const val threeten = "com.jakewharton.threetenabp:threetenabp:1.2.3"
 }
