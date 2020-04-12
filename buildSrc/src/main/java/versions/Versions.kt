cc3690d82748e8e9325daf11ad540f12a94505be
takechee <takechee14@gmail.com>
2020-04-12 14:23:05 +0900

    バージョン管理

--- buildSrc/src/main/java/versions/Versions.kt
+++ buildSrc/src/main/java/versions/Versions.kt
@@ -5,13 +5,17 @@ object Versions {
     const val compileSdkVersion = 29
     const val minSdkVersion = 24
     const val targetSdkVersion = 29
-    
-    const val versionCode = AppVersion.major * 10000 + AppVersion.minor * 100 + AppVersion.patch
-    const val versionName = "${AppVersion.major}.${AppVersion.minor}.${AppVersion.patch}"
+
+    const val versionCode: Int = AppVersion.code
+    const val versionName: String = AppVersion.name
 }
 
 private object AppVersion {
-    const val major = 1
-    const val minor = 0
-    const val patch = 0
+    private const val major: Int = 1
+    private const val minor: Int = 0
+    private const val patch: Int = 0
+    private const val build: Int = 0
+
+    internal const val code: Int = (major * 10000 + minor * 100 + patch) * 100 + build
+    internal const val name: String = "$major.$minor.$patch"
 }
