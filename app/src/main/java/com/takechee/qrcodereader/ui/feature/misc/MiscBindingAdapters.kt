6bc80c598d6a6c9fe49744b78293d2a7da6bffee
takechee <takechee14@gmail.com>
2020-04-15 21:20:47 +0900

    ビルドタイプ表記追加

--- app/src/main/java/com/takechee/qrcodereader/ui/feature/misc/MiscBindingAdapters.kt
+++ app/src/main/java/com/takechee/qrcodereader/ui/feature/misc/MiscBindingAdapters.kt
@@ -3,8 +3,21 @@ package com.takechee.qrcodereader.ui.feature.misc
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.takechee.qrcodereader.R
+import java.util.*
 
 @BindingAdapter("misc_versionName", "misc_gitCommitHash")
 fun setVersionName(view: TextView, versionName: String, gitCommitHash: String) {
-    view.text = view.resources.getString(R.string.version_name, versionName, gitCommitHash)
+    view.text = view.resources.getString(
+        R.string.version_name,
+        versionName,
+        gitCommitHash
+    )
+}
+
+@BindingAdapter("misc_buildType")
+fun setBuildType(view: TextView, buildType: String) {
+    view.text = view.resources.getString(
+        R.string.build_type,
+        buildType.toUpperCase(Locale.getDefault())
+    )
 }
