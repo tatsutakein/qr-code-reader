90f9d02fd11c6e6837cac5599ae7a90169954c53
takechee <takechee14@gmail.com>
2020-04-16 20:35:46 +0900

    jsourpの導入

--- buildSrc/src/main/java/dependencies/Dependencies.kt
+++ buildSrc/src/main/java/dependencies/Dependencies.kt
@@ -77,4 +77,5 @@ object Dependencies {
     const val threeten = "com.jakewharton.threetenabp:threetenabp:1.2.3"
     const val insetterDbx = "dev.chrisbanes:insetter-dbx:0.2.1"
     const val insetterKtx = "dev.chrisbanes:insetter-ktx:0.2.1"
+    const val jsourp = "org.jsoup:jsoup:1.13.1"
 }
