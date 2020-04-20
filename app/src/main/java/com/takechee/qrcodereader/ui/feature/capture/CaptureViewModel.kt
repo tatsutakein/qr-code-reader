30e25560afa6dc01c829ef52d54a7117fa98ec3b
takechee <takechee14@gmail.com>
2020-04-20 22:18:36 +0900

    読み取りに間隔をつける

--- app/src/main/java/com/takechee/qrcodereader/ui/feature/capture/CaptureViewModel.kt
+++ app/src/main/java/com/takechee/qrcodereader/ui/feature/capture/CaptureViewModel.kt
@@ -12,6 +12,8 @@ import com.takechee.qrcodereader.result.Event
 import com.takechee.qrcodereader.result.fireEvent
 import com.takechee.qrcodereader.ui.Navigator
 import com.takechee.qrcodereader.ui.common.base.BaseViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -20,9 +22,15 @@ class CaptureViewModel @Inject constructor(
     private val repository: ContentRepository
 ) : BaseViewModel(), BarcodeCallback, Navigator by navigator {
 
+    companion object {
+        private const val DELAY_MS: Long = 500
+    }
+
     private val _event = MutableLiveData<Event<CaptureEvent>>()
     val event: LiveData<Event<CaptureEvent>>
         get() = _event.distinctUntilChanged()
+
+    private var captureResultJob: Job? = null
 
 
     // =============================================================================================
@@ -31,11 +39,14 @@ class CaptureViewModel @Inject constructor(
     //
     // =============================================================================================
     override fun barcodeResult(result: BarcodeResult?) {
-        viewModelScope.launch {
+        if (captureResultJob?.isActive == true) return
+
+        captureResultJob = viewModelScope.launch {
             val resultText = result?.text ?: return@launch
             repository.insertCaptureText(resultText) { contentId ->
                 navigator.navigateToDetail(contentId)
             }
+            delay(DELAY_MS)
         }
     }
 
