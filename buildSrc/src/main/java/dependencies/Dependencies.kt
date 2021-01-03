20c374f5fafd2951fb5faf29ff471fcf68d375c2
takechee <takechee14@gmail.com>
2021-01-03 10:56:10 +0900

    kotlin

--- buildSrc/src/main/java/dependencies/Dependencies.kt
+++ buildSrc/src/main/java/dependencies/Dependencies.kt
@@ -19,6 +19,7 @@ object Dependencies {
         object Coroutines {
             private const val version = "1.4.2"
             const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
+            const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
         }
     }
 
